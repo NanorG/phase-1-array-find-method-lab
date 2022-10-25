5d24c2e67b459718cd062e280418e80e0ac64f6f
NanorGosdanian <nanorgosdanian@gmail.com>
2022-10-25 20:42:53 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(arr){
+  /*arr.find(record => { 
+     if(record.result === "W") 
+       return true; 
+     else 
+       return false  
+ }) */
+ let winner = arr.find(record => record.result === 'W');
+ if (winner ) return winner.year
+}
